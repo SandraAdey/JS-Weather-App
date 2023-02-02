75e7d95729fdeb941440fed178fe094b93f8f25f
Sandra Adey <sandraadeyinka@gmail.com>
2023-02-02 19:59:12 +0000

    Added the search engine

--- app.js
+++ app.js
@@ -45,9 +45,21 @@ function displayTemperature(response) {
 
 }
 
-let apiKey = "b22a22ad758155a3cdfe4cbd942b4898";
-let city= "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+   let apiKey = "b22a22ad758155a3cdfe4cbd942b4898";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value)
+    
+}
+
+search("New York");
 
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
